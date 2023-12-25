de982ef8efea2c04359e90f92907051fc861d3e9
papuna <papunapetriashvili1@gmail.com>
2023-12-25 17:10:20 +0400

    type casting

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        long roundedNumbers = Math.round(numberToBeRounded);
+        System.out.println(roundedNumbers);
     }
 }
